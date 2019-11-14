ae7d1c2e6f44816831d26d00ed7c756dcd9030ed
tigist endale <tigistendale6@gmail.com>
2019-11-14 18:41:21 +0300

    sd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 64735;
 const r = require("rethinkdb");
 var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // support json encoded bodies
